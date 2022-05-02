45a1325786a81b1faf68ba4e118a0a96e4525ddd
TBSten <programmingcafeteria@gmail.com>
2022-05-02 10:28:07 +0900

    merge dev

--- deploy.sh
+++ deploy.sh
@@ -7,16 +7,26 @@ if [ $NOW_BRANCH != $BRANCH ] ; then
     exit 1
 fi
 
+COMMAND_CNT=7
 
 git add .
+echo "[1/$COMMAND_CNT] command finished"
 git commit
+echo "[2/$COMMAND_CNT] command finished"
 
 git checkout main
+echo "[3/$COMMAND_CNT] command finished"
 
 git merge dev
+echo "[4/$COMMAND_CNT] command finished"
 git add .
+echo "[5/$COMMAND_CNT] command finished"
 git commit -m "merge dev"
+echo "[6/$COMMAND_CNT] command finished"
 
 git checkout dev
+echo "[7/$COMMAND_CNT] command finished"
+
+echo "finish"
 
 
